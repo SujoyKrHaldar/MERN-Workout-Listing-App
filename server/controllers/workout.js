6d9249ab975b48a9ad5b6aeb83f85775dc3b81b4
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-06-21 02:44:52 +0530

    Server - Schema creation and Post method testing.

--- server/controllers/workout.js
+++ server/controllers/workout.js
@@ -1,7 +1,18 @@
+const workoutSchema = require("../models/workoutSchema");
+
 const workout = {
   getALlData: (req, res) => res.status(200).json({ message: "Get all data" }),
   getOneData: (req, res) => res.status(200).json({ message: "Get one data" }),
-  postData: (req, res) => res.status(200).json({ message: "Post a data" }),
+
+  postData: async (req, res) => {
+    try {
+      const newData = await workoutSchema.create(req.body);
+      res.status(201).json({ message: "Entry created.", newData });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
   updateData: (req, res) => res.status(200).json({ message: "Update a data" }),
   deleteData: (req, res) => res.status(200).json({ message: "Delete a data" }),
 };
